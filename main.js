266dcebdc4d2aa85602895c9555c03d5e789dc25
Balkrishna Agarwal <agarwalbalkrishnaindia@gmail.com>
2020-08-12 19:35:31 +0530

    website initiation

---
+++ main.js
@@ -0,0 +1,107 @@
+// // Set the date we're counting down to
+// var countDownDate = new Date("Aug 22, 2020 10:00:00").getTime();
+
+// // Update the count down every 1 second
+// var x = setInterval(function() {
+
+//   // Get today's date and time
+//   var now = new Date().getTime();
+
+//   // Find the distance between now and the count down date
+//   var distance = countDownDate - now;
+
+//   // Time calculations for days, hours, minutes and seconds
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+//   // Display the result in the element with id="demo"
+//   document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+//   + minutes + "m " + seconds + "s ";
+// }, 1000);
+
+//Typing animation with alternating text in the main-header
+const typedTextSpan = document.querySelector(".type-anime")
+const cursorSpan = document.querySelector(".cursor-anime")
+
+const textArray = document.querySelector(".type-anime").getAttribute('data-elements').split(','); //["WEB DEVELOPERS", "CREATIVE", "AMBITIOUS"];
+const typingDelay = 100;
+const erasingDelay = 100;
+const newTextDelay = 1500;
+let textArrayIndex = 0;
+let charIndex = 0; 
+
+function type(){
+    if(charIndex < textArray[textArrayIndex].length){
+        if(!cursorSpan.classList.contains('.typing')) cursorSpan.classList.add('.typing')
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    }
+    else{
+        cursorSpan.classList.remove('.typing');
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase(){
+    if (charIndex > 0){
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        textArrayIndex++;
+        if(textArrayIndex>=textArray.length) textArrayIndex=0;
+        setTimeout(type, typingDelay+200);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    if (textArray.length) setTimeout(type,500);
+})
+
+
+
+function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+    
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  }
+  
+  function initializeClock(id, endtime) {
+    const clock = document.getElementById(id);
+    const daysSpan = clock.querySelector('.days');
+    const hoursSpan = clock.querySelector('.hours');
+    const minutesSpan = clock.querySelector('.minutes');
+    const secondsSpan = clock.querySelector('.seconds');
+  
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+  
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+  
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+  
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+  }
+  
+  const deadline = 'August 22 2020 10:00:00 GMT+0530';
+  initializeClock('clockdiv', deadline);
